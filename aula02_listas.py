49a0724c28d2017b8a89c663b97aa854a4e65429
Vinicius <vinicius.silva@outlook.com>
2019-01-29 12:29:03 -0200

    Aula 02

--- aula02_listas.py
+++ aula02_listas.py
@@ -5,17 +5,16 @@ import json
 lista = json.loads(requests.get("https://bit.ly/2RXuXsD").text)
 # print(lista)
 
-dicionario = {lista[0]:0}
+dicionario = {}
 
 for elemento in lista:
-    count = 0
     if elemento in dicionario:
-        count = dicionario[elemento] + 1
-        dicionario[elemento] = count
+        dicionario[elemento] += 1
     else:
         dicionario[elemento] = 1
 
 print(dicionario)
+
 #    dicionario[elemento] = lista.count[elemento]
 
 # lista.count[13]
